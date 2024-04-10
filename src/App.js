9d87d43dd7830811fa407ac0ee510f1f5666ebf8
ascurious <ascurious8&@gmail.com>
2024-04-10 15:28:21 +0330

    file and folder structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "./components/Layout/Layout";
+
+const App = (props)=>{
+    return(
+        <div>
+            <Layout>
+                <p>this is test</p>
+            </Layout>
+        </div>
+    )
+};
+
+export default App;
