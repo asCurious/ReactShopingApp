1f7e3f9288572a46243334fa8f2eb2439f7e7276
Ali Shabani <54898257+asCurious@users.noreply.github.com>
2024-04-15 15:40:56 +0000

    create first state

--- src/containers/Shopping/Shopping.js
+++ src/containers/Shopping/Shopping.js
@@ -2,6 +2,15 @@ import React from "react";
 import Wrapper from "../../hoc/wrapper";
 
 const Shopping = (props)=>{
+    state= {
+        products:{
+            product1: 0,
+            product2: 0,
+            product3: 0,
+            product4: 0,
+        },
+        totalPrice: 0,
+    }
     return(
         <Wrapper>
             <p>این یک متن تست است.</p>
