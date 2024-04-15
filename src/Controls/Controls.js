c779ed473ac152e91393debd01d4786df27314e3
Ali Shabani <54898257+asCurious@users.noreply.github.com>
2024-04-15 15:45:49 +0000

    create products list

--- src/Controls/Controls.js
+++ src/Controls/Controls.js
@@ -2,6 +2,12 @@ import React from "react";
 import "./Controls.css"
 
 const Controls = (props)=>{
+    const products = [
+        {title: "product1" , type: "product1"},
+        {title: "product2" , type: "product2"},
+        {title: "product3" , type: "product3"},
+        {title: "product4" , type: "product4"},
+    ]
     return(
         <div className="controls">
             
