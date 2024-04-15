df9accaa1a5b84d2e297904bda3eb7ac9ee6d7b8
Ali Shabani <54898257+asCurious@users.noreply.github.com>
2024-04-15 16:43:43 +0000

    edit products title to persian

---
+++ src/Controls/Controls.css/Controls.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Controls.css"
+import Builder from "../Builder/Builder";
+
+const Controls = (props)=>{
+    const products = [
+        {title: "محصول1" , type: "product1"},
+        {title: "محصول2" , type: "product2"},
+        {title: "محصول3" , type: "product3"},
+        {title: "محصول4ض" , type: "product4"},
+    ]
+    return(
+        <div className="controls">
+            {products.map((item)=>{
+                return <Builder key={item.title} title={item.title} />
+            })}
+            
+        </div>
+    )
+}
+
+export default Controls
