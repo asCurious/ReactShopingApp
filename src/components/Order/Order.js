4f565756ed94dab8e1fc696f15cfd74eb36295fd
ascurious <ascurious8&@gmail.com>
2024-04-16 23:14:57 +0330

    create order component

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Wrapper from "../../hoc/wrapper";
+
+const Order = (props) =>{
+    return(
+        <Wrapper>
+            
+        </Wrapper>
+    )
+}
+
+export default Order;
