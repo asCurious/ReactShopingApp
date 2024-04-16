7957686c5012240ab14b82db1935285f45e9df5b
ascurious <ascurious8&@gmail.com>
2024-04-16 23:23:02 +0330

    summery of order

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,9 +2,17 @@ import React from "react";
 import Wrapper from "../../hoc/wrapper";
 
 const Order = (props) =>{
+    const summery = Object.keys(props.products).map((item) =>{
+        return (
+            <li key={item}>
+                {item} : {props.products[item]}
+            </li>
+        )
+    })
     return(
         <Wrapper>
-            
+            <h3>سفارش</h3>
+            <ul>{summery}</ul>
         </Wrapper>
     )
 }
