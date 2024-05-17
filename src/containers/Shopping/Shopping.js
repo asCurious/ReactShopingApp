61409e89181b226f3589811b0b1524be2dd212d9
ascurious <ascurious8&@gmail.com>
2024-05-17 12:23:07 +0330

    create firebase post

--- src/containers/Shopping/Shopping.js
+++ src/containers/Shopping/Shopping.js
@@ -3,6 +3,7 @@ import Wrapper from "../../hoc/wrapper";
 import Controls from "../../Controls/Controls/Controls";
 import Modal from "../../components/UI/Modal/Modal";
 import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
 
 const prices = {
     product1: 10000,
@@ -53,7 +54,19 @@ const Shopping = (props)=>{
       setState(prevState => ({ ...prevState, purchased: false }));
     }
     const purchasedContinueHandler = ()=>{
-      console.log("purchasedContinueHandler")
+      const order = {
+        products: state.products,
+        totalPrice: state.totalPrice,
+        customer: {
+          name: "Ali",
+          email: "ascurious87@gmail.com"
+        }
+      }
+      axios.post("/orders.json", order).then((response)=>{
+        console.log(response)
+      }).catch((error)=>{
+        console.log(error)
+      })
     }
     return(
         <Wrapper>
